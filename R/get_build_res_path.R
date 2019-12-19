97f193e97b295dd86768a2e1679304031825ad30
mgenu <mathieu.genu@univ-lr.fr>
2019-12-19 17:37:40 +0100

    - start of documentation - correction on gsub of "/R/"

--- R/get_build_res_path.R
+++ R/get_build_res_path.R
@@ -1,12 +1,25 @@
+#' get and build result path
+#'
+#' @return 2 actions are made
+#'         \enumerate{
+#'           \item gives the path name of the res folder mirror of the path of the R script
+#'           \item create the res path corresponding of the R script
+#'           \item graph : Barplot of detections depending of distance, with confindence
+#'           estimated with mcmc chains.
+#'         }
+#' @examples
+#'
+#'
 #' @export
 
-get_build_res_path <- function(r_script_path) {
+
+get_build_res_path <- function() {
 
   # get script path
   script_path <- rstudioapi::getSourceEditorContext()$path
 
   # delete root and .R extension
-  path_w_root <- str_extract(pattern = "R/(.*)",string = script_path)
+  path_w_root <- str_extract(pattern = "/R/(.*)",string = script_path)
   path_w_root <- str_extract(pattern = "^.*(?=(.R))",string = path_w_root)
 
   # res path to create
